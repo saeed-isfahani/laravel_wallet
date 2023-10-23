dd45899c73b07cc2bf73d65716899e1b02e5c453
saeed <saeed.com2000@gmail.com>
2023-10-23 20:53:35 +0330

    feat: make relation between currencies unique key and other tables

---
+++ database/migrations/2023_10_23_171628_change_currency_column_to_unsigned_in_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreign('currency')
+                ->references('key')
+                ->on('currencies')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['currency']);
+            $table->string('currency')->change();
+        });
+    }
+};

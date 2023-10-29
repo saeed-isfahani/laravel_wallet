ccab797cdf78971e99c20e2694909346c157aa82
saeed <saeed.com2000@gmail.com>
2023-10-29 14:15:41 +0330

    feat: use CurrencyFactory in PaymentFactoryinstead of array

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\currency>
@@ -17,7 +19,12 @@ class CurrencyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => Str::random(5),
+            'key' => Str::random(5),
+            'symbol' => Str::random(5),
+            'iso_code' => Str::random(5),
+            'is_active' => true,
+            'user_id' => User::factory()
         ];
     }
 }
--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enums\Payments\PaymentStatus;
+use App\Models\Currency;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -22,10 +23,9 @@ class PaymentFactory extends Factory
         return [
             'unique_id' => Str::uuid(),
             'user_id' => User::factory(1)->create()[0]->id,
-            'amount' => rand(100, 9999),
+            'amount' => fake()->randomDigit(),
             'status' =>  PaymentStatus::PENDING,
-            // TODO use currency model instead of array
-            'currency_key' => $this->faker->randomElement(['rial', 'dollar', 'euro', 'yuan'])
+            'currency_key' => Currency::factory(1)->create()[0]->key
         ];
     }
 }

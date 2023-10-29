e3d84e1c4089668a3d033f2af8f7f95106c610b2
saeed <saeed.com2000@gmail.com>
2023-10-29 13:01:43 +0330

    feat: implemet pagination in collections

--- app/Http/Resources/PaymentCollection.php
+++ app/Http/Resources/PaymentCollection.php
@@ -7,6 +7,23 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PaymentCollection extends ResourceCollection
 {
+    private $pagination;
+
+    public function __construct($resource)
+    {
+        $this->pagination = [
+            'total' => $resource->total(),
+            'count' => $resource->count(),
+            'per_page' => $resource->perPage(),
+            'current_page' => $resource->currentPage(),
+            'total_pages' => $resource->lastPage()
+        ];
+
+        $resource = $resource->getCollection();
+
+        parent::__construct($resource);
+    }
+
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +31,9 @@ class PaymentCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'docs' => $this->collection,
+            'pagination' => $this->pagination
+        ];
     }
 }
